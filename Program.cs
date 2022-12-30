815c3aba6c719817a10efae34045c88107fb0c21
emanshaker17 <121675531+emanshaker17@users.noreply.github.com>
2022-12-30 15:32:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       int n1, n2;
+        Console.WriteLine("Get All Perfect In Range of Between two Number");
+        Console.Write("Enter Starting Number : ");
+        n1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter Ending Number : ");
+        n2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Below are the perfect number between " + n1 + " and " + n2);
+
+        for (int i = n1; i <= n2; i++)
+        {
+            int sum = 0;
+            for (int j = 1; j < i; j++)
+            {
+                if (i % j == 0)
+                    sum = sum + j;
+            }
+            if (sum == i)
+                Console.WriteLine(i);
+        }
+
+        Console.ReadKey();
+    }
+}
